22d5e90e44d1e53b680811809383e04674a558c6
Vitaly Kulin <vitaskul@mail.ru>
2022-09-18 19:05:23 +0300

    Добавление конструкторов в OffsetSettings
    Добавлен конструктор без аргументов и конструктор, принимающий
    4 аргумента - относительное смещение с 4х сторон.

--- cplotlib/commontypes.h
+++ cplotlib/commontypes.h
@@ -5,10 +5,20 @@ namespace cplt
 {
     struct OffsetSettings
     {
-        double top = 0.1;
-        double bottom = 0.15;
-        double right = 0.1;
-        double left = 0.2;
+        OffsetSettings()
+        {
+            top = 0.1;
+            bottom = 0.15;
+            right = 0.1;
+            left = 0.2;
+        }
+        OffsetSettings(double top, double bottom, double right, double left)
+        : top(top), bottom(bottom), right(right), left(left)
+        {}
+        double top;
+        double bottom;
+        double right;
+        double left;
     };
     using lim_t = std::pair<double,double>;
     using ticks_t = std::vector<double>;
