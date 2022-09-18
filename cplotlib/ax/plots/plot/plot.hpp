844afbad1f4c2ce3653ea8f1745e790940e2a823
Vitaly Kulin <vitaskul@mail.ru>
2022-09-18 17:13:05 +0300

    Добавлены сигнатуры интерфейсов пополнения контейнера в Plot
    Для динамической работы с объектом Plot и отрисовки процессов в реальном
    времени необходимо иметь возможность пополнять
    контейнер point'ов "на горячую".
    В пользовательском коде будет осуществляться получение
    shared_ptr объекта класса Plot через объект класса Ax методом
    getPlot(number). Также указатель на объект класса Plot будет
    возвращаться при построении нового графика Plot.
    При добавлении новой точки графика НЕ подразумевается
    уведомления подписчиков для вызова refresh(), т.к. нет необходимости.

--- cplotlib/ax/plots/plot/plot.hpp
+++ cplotlib/ax/plots/plot/plot.hpp
@@ -19,7 +19,14 @@ namespace plots
             std::shared_ptr<cplt::OffsetSettings> offsetSettings,
             std::shared_ptr<cplt::lim_t> xlim,
             std::shared_ptr<cplt::lim_t> ylim);
-        void draw() override;
+        void draw() override; //not for users
+
+        void addPointToBegin(cv::Point2d p);
+        void addPointToBegin(double x, double y);
+        void addPointToTail(cv::Point2d p);
+        void addPointToTail(double x, double y);
+        int getTotalPoints(); // Возвращает общее число точек графика
+        void insertPoint(int position, cv::Point2d point); //встав. точку в согласно position
     private:
         plotsSettings::PlotSettings settings;
         std::vector<double> x, y;
