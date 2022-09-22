a24192a11d51af3524dc059fb76f328c82e3dc71
Vitaly Kulin <vitaskul@mail.ru>
2022-09-23 00:03:43 +0300

    Добавлена заготовка класса legend

---
+++ cplotlib/ax/ax_components/legend/legend.hpp
@@ -0,0 +1,18 @@
+#ifndef LEGEND_HPP
+#define LEGEND_HPP
+
+
+#include <memory>
+#include <opencv2/opencv.hpp>
+#include "../../../tools/colors/colors.hpp"
+#include "../axobject.hpp"
+
+namespace ax_components
+{
+    class Legend : public AxObject
+    {
+    
+    };
+}
+
+#endif // LEGEND_HPP
